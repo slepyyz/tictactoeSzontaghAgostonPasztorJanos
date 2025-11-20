d7c3dce8b45c63cab52247971f6edc1ce45c2163
Bencecigany <pasztorbence04@gmail.com>
2025-11-20 14:02:06 +0100

    ellenőrzés,géplépés bekérés kész

--- feladatok.py
+++ feladatok.py
@@ -1,14 +1,85 @@
+import random
+
+palya = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+]
+
+jatek_megy = 1
+lepesek_szama = 0
+nyertes = "nincs"
+
 def kiiras():
-    print()
-    print("    1   2   3")
-    print("  ┏━━━┳━━━┳━━━┓")
-    print("A ┃   ┃   ┃   ┃")
-    print("  ┣━━━╋━━━╋━━━┫")
-    print("B ┃   ┃   ┃   ┃")
-    print("  ┣━━━╋━━━╋━━━┫")
-    print("C ┃   ┃   ┃   ┃")
-    print("  ┗━━━┻━━━┻━━━┛")
-    print()
+    print(palya[0])
+    print(palya[1])
+    print(palya[2])
 
+def bekeres():
+    global palya, lepesek_szama
+    print("\n--- Te jössz! ---")
+    
+    te_keresel = 1
+    while te_keresel == 1:
+        # Ha betűt írsz, a program leáll Csak számot írj.
+        sor = int(input("Sor (0, 1, 2): "))
+        oszlop = int(input("Oszlop (0, 1, 2): "))
+        
+        # Megnézzük, hogy a számok jók-e (0 és 2 között)
+        if sor < 0:
+            print("Hiba: 0-nál kisebb számot adtál.")
+        elif sor > 2:
+            print("Hiba: 2-nél nagyobb számot adtál.")
+        elif oszlop < 0:
+            print("Hiba: 0-nál kisebb számot adtál.")
+        elif oszlop > 2:
+            print("Hiba: 2-nél nagyobb számot adtál.")
+        else:
+            if palya[sor][oszlop] == "-":
+                palya[sor][oszlop] = "X"
+                lepesek_szama = lepesek_szama + 1
+                te_keresel = 0 
+            else:
+                print("Hiba: Ez a hely már foglalt!")
 
-    print("━━━━━━━━━━")
+def geplepes():
+    global palya, lepesek_szama
+    print("\n--- A gép jön ---")
+    
+    gep_keres = 1
+    while gep_keres == 1:
+        gs = random.randint(0, 2)
+        go = random.randint(0, 2)
+        
+        if palya[gs][go] == "-":
+            palya[gs][go] = "O"
+            lepesek_szama = lepesek_szama + 1
+            gep_keres = 0
+
+def ellenorzes():
+    nyertes = "nincs"
+    # --- ELLENŐRZÉS: NYERTÉL-E? ---
+    # Sorok
+    if palya[0][0] == "X" and palya[0][1] == "X" and palya[0][2] == "X": nyertes = "TE"
+    if palya[1][0] == "X" and palya[1][1] == "X" and palya[1][2] == "X": nyertes = "TE"
+    if palya[2][0] == "X" and palya[2][1] == "X" and palya[2][2] == "X": nyertes = "TE"
+    # Oszlopok
+    if palya[0][0] == "X" and palya[1][0] == "X" and palya[2][0] == "X": nyertes = "TE"
+    if palya[0][1] == "X" and palya[1][1] == "X" and palya[2][1] == "X": nyertes = "TE"
+    if palya[0][2] == "X" and palya[1][2] == "X" and palya[2][2] == "X": nyertes = "TE"
+    # Átlók
+    if palya[0][0] == "X" and palya[1][1] == "X" and palya[2][2] == "X": nyertes = "TE"
+    if palya[0][2] == "X" and palya[1][1] == "X" and palya[2][0] == "X": nyertes = "TE"
+    
+    # --- ELLENŐRZÉS: NYERT-E A GÉP? ---
+    if palya[0][0] == "O" and palya[0][1] == "O" and palya[0][2] == "O": nyertes = "GEP"
+    if palya[1][0] == "O" and palya[1][1] == "O" and palya[1][2] == "O": nyertes = "GEP"
+    if palya[2][0] == "O" and palya[2][1] == "O" and palya[2][2] == "O": nyertes = "GEP"
+    if palya[0][0] == "O" and palya[1][0] == "O" and palya[2][0] == "O": nyertes = "GEP"
+    if palya[0][1] == "O" and palya[1][1] == "O" and palya[2][1] == "O": nyertes = "GEP"
+    if palya[0][2] == "O" and palya[1][2] == "O" and palya[2][2] == "O": nyertes = "GEP"
+    if palya[0][0] == "O" and palya[1][1] == "O" and palya[2][2] == "O": nyertes = "GEP"
+    if palya[0][2] == "O" and palya[1][1] == "O" and palya[2][0] == "O": nyertes = "GEP"
+    
+    return nyertes
+
--- main.py
+++ main.py
@@ -4,6 +4,17 @@ print("=" * 30)
 print("         AMŐBA JÁTÉK ")
 print("=" * 30)
 
-feladatok.kiiras()
+ujra_jatszik = True
 
-print("Üdvözöllek az Amőba játékban!")
+while ujra_jatszik:
+    eredmeny = feladatok.jatek_futtat()
+    
+    print("\n" + "=" * 30)
+    if eredmeny == "TE":
+        print(" GYŐZELEM! Te nyertél!")
+    elif eredmeny == "GEP":
+        print(" A gép nyert! Próbáld újra!")
+    elif eredmeny == "DONTETLEN":
+        print(" Döntetlen! Jó játék volt!")
+    
+   
