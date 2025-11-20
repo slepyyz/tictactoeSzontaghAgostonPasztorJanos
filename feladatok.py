6e2e8c467d8ba9920808e390f4ccfb014bff1d28
Bencecigany <pasztorbence04@gmail.com>
2025-11-20 13:40:25 +0100

    agoston elhagyott egyedul maradtam

---
+++ feladatok.py
@@ -0,0 +1,14 @@
+def kiiras():
+    print()
+    print("    1   2   3")
+    print("  ┏━━━┳━━━┳━━━┓")
+    print("A ┃   ┃   ┃   ┃")
+    print("  ┣━━━╋━━━╋━━━┫")
+    print("B ┃   ┃   ┃   ┃")
+    print("  ┣━━━╋━━━╋━━━┫")
+    print("C ┃   ┃   ┃   ┃")
+    print("  ┗━━━┻━━━┻━━━┛")
+    print()
+
+
+    print("━━━━━━━━━━")
---
+++ main.py
@@ -0,0 +1,9 @@
+import feladatok
+
+print("=" * 30)
+print("         AMŐBA JÁTÉK ")
+print("=" * 30)
+
+feladatok.kiiras()
+
+print("Üdvözöllek az Amőba játékban!")
